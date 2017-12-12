29f48c31b4daa8a86943cc9344619c271548f644
Abhishek Kumar Jain <abhishekkumar.jain@outlook.com>
2017-12-12 16:06:56 +0530

    created the console application with pnp core extension method.

--- PnPExtentionMethod/Program.cs
+++ PnPExtentionMethod/Program.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using Microsoft.SharePoint.Client;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security;
 using System.Text;
 using System.Threading.Tasks;
+//using OfficeDevPnP.Core;
 
 namespace PnPExtentionMethod
 {
@@ -10,6 +13,14 @@ namespace PnPExtentionMethod
     {
         static void Main(string[] args)
         {
+            string userName = "";
+            SecureString password = new SecureString();
+
+            using(var ctx = new ClientContext(""))
+            {
+                ctx.Credentials = new SharePointOnlineCredentials(userName, password);
+                var list = ctx.Web.Lists[0];
+            }
         }
     }
 }
